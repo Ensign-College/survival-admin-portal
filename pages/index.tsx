8b748d9f30baf66e0e3eaaffba606823754c914b
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-06-29 11:14:56 -0600

    feature: deploy with netlify

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 import {useState, useEffect, ChangeEvent, FormEvent} from 'react';
 import EditModal from "./EditModal";
-
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
+import {SUPABASE_API_KEY, SUPABASE_URL} from "../services/supabaseClients";
 
 const supabase = createClient(SUPABASE_URL as string, SUPABASE_API_KEY as string);
 
@@ -32,8 +30,18 @@ const HomePage = () => {
         }
     };
 
+    const resetForm = () => {
+        setForm({
+            title: '',
+            image_logo: '',
+            card_detail_id: 0,
+            card_detail_text: '',
+            card_detail_pictures: ''
+        });
+    };
+
     const handleDelete = async (id: number) => {
-        const { error } = await supabase.from('card').delete().match({ id });
+        const {error} = await supabase.from('card').delete().match({id});
         if (error) {
             console.error('Error deleting card:', error);
         } else {
@@ -57,26 +65,33 @@ const HomePage = () => {
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const newCard = { title: form.title, image_logo: form.image_logo };
 
-        const cardResponse = await supabase.from('card').insert([newCard]);
-        const firstCard = cardResponse.data ? cardResponse.data[0] : null;
-        const cardError = cardResponse.error;
+        if (form.card_detail_text.trim() === '') {
+            alert('Card detail text is required');
+            return;
+        }
+        const newCard = {title: form.title, image_logo: form.image_logo};
+
+        const { data, error } = await supabase.from('card').insert([newCard]).select();
+        const firstCard = data ? data[0] : null;
+        const cardError = error;
 
         if (cardError || !firstCard) {
-            console.error('Error inserting new card:', cardError);
+            alert('Error inserting new card:', cardError);
+            console.error(cardError)
             return;
         }
 
         const newCardDetails = {
-            title: form.title, // Use the same title for card details
+            title: form.title,
             pictures: form.card_detail_pictures.split(','),
             card_id: firstCard.id,
             text: form.card_detail_text
         };
 
         const cardDetailResponse = await supabase.from('card_details').insert([newCardDetails]);
-        const firstCardDetail = cardDetailResponse.data ? cardDetailResponse.data[0] : null;
+        const cardDetailData = cardDetailResponse.data;
+        const firstCardDetail = cardDetailData ? cardDetailData[0] : null;
         const cardDetailError = cardDetailResponse.error;
 
         if (cardDetailError || !firstCardDetail) {
@@ -84,7 +99,14 @@ const HomePage = () => {
             return;
         }
 
-        setCards([...cards, { ...firstCard, card_details: firstCardDetail }]);
+        // Update card_detail_id in the card table
+        await supabase.from('card').update([{card_detail_id: firstCardDetail.id}])
+            .eq('id', firstCard.id);
+
+        const updatedCard = {...firstCard, card_detail_id: firstCardDetail.id};
+
+        setCards([...cards, updatedCard]);
+        resetForm();
     };
 
 
@@ -144,7 +166,8 @@ const HomePage = () => {
                     )}
                 </div>
                 <div className="mb-4">
-                    <label className="block mb-2 text-sm font-bold text-gray-700">Card Detail Pictures (comma separated URLs):</label>
+                    <label className="block mb-2 text-sm font-bold text-gray-700">Card Detail Pictures (comma separated
+                        URLs):</label>
                     <input
                         type="text"
                         name="card_detail_pictures"
@@ -162,24 +185,30 @@ const HomePage = () => {
             </form>
             <div className="space-y-4">
                 {cards.map((card, index) => (
-                    <div key={index} className="flex items-center p-4 border rounded shadow-lg justify-between" style={{ minWidth: '300px' }}>
+                    <div key={index} className="flex items-center p-4 border rounded shadow-lg justify-between"
+                         style={{minWidth: '300px'}}>
                         <div className="flex items-center">
                             {card.image_logo === "https://example.com/logo.png" ? (
                                 <div className="flex items-center justify-center w-16 h-16 mr-4 bg-gray-200">
                                     <span className="text-gray-400">No Image</span>
                                 </div>
                             ) : (
-                                <img src={card.image_logo} alt={card.title} className="object-cover w-16 h-16 mr-4 max-w-full max-h-full"/>
+                                <img src={card.image_logo} alt={card.title}
+                                     className="object-cover w-16 h-16 mr-4 max-w-full max-h-full"/>
                             )}
                             <h2 className="text-xl flex-shrink-0">{card.title}</h2>
                         </div>
                         <div className="button-container">
-                            <button onClick={() => handleEdit(card.id)} className="px-2 py-1 text-slate-400 bg-transparent hover:bg-green-500 rounded hover:text-white">
+                            <button onClick={() => handleEdit(card.id)}
+                                    className="px-2 py-1 text-slate-400 bg-transparent hover:bg-green-500 rounded hover:text-white">
                                 Edit
                             </button>
-                            <button onClick={() => handleDelete(card.id)} className="px-2 py-1 text-white bg-transparent hover:bg-red-500 rounded hover:text-white">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 fill-red-600">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            <button onClick={() => handleDelete(card.id)}
+                                    className="px-2 py-1 text-white bg-transparent hover:bg-red-500 rounded hover:text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke="currentColor" className="h-6 w-6 fill-red-600">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                 </svg>
                             </button>
                         </div>
@@ -188,7 +217,7 @@ const HomePage = () => {
                 ))}
             </div>
             {isEditModalOpen && (
-                <EditModal card={currentCard} onClose={() => setIsEditModalOpen(false)} onSubmit={handleEdit} />
+                <EditModal card={currentCard} onClose={() => setIsEditModalOpen(false)} onSubmit={handleEdit}/>
             )}
         </div>
     );
---
+++ services/supabaseClients.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+export const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
+
+
