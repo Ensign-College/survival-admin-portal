e5fd0eec992599a99e5fd4887eac8bb612a8b6f3
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-07-06 12:42:22 -0600

    development: start login form

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
+
+export const supabase = createClient(SUPABASE_URL as string, SUPABASE_API_KEY as string);
