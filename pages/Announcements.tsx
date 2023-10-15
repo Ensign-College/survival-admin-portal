857d7d6872c1541687f5e9120ad09cd456cdaf15
Iana Sanduta <iana.sandutsa@gmail.com>
2023-10-14 19:34:23 -0600

    Feat:Added the Announcements page
    - Added Announcements page
    - Added Notifications to this page
    - Notifications are successfully sent to the Mobile app

---
+++ pages/Announcements.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import PictureInput from "../components/inputs/PictureInput";
+import {
+  handleSendNotification,
+  handleSendImageNotification,
+} from "../services/notificationService";
+
+const Announcements: React.FC = () => {
+  const [topic, setTopic] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleCallNotifications = async () => {
+    try {
+      // Call the notification function with the input values
+      if (imageUrl) {
+        // If imageUrl is not empty, call the handleSendImageNotification function
+        await handleSendImageNotification(topic, title, body, imageUrl);
+      } else {
+        // If imageUrl is empty, call the handleSendNotification function
+        await handleSendNotification(topic || "all", title, body);
+      }
+      // Optionally, you can clear the input fields here
+      setTopic("");
+      setTitle("");
+      setBody("");
+      setImageUrl("");
+    } catch (error) {
+      // Handle any errors here
+      console.error("Error sending notification:", error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-white flex justify-center">
+      <div className="w-1/2 p-8 flex flex-col">
+        <p className="text-2xl font-semibold">Announcements</p>
+        <label htmlFor="topic">Topic:</label>
+        <input
+          className="bg-white shadow-md rounded px-8 pt-3 mt-3 pb-3 mb-4"
+          type="text"
+          id="topic"
+          name="topic"
+          placeholder="Notification topic"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          className="bg-white shadow-md rounded px-8 pt-3 mt-3 pb-3 mb-4"
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Notification title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="body">Text:</label>
+        <input
+          className="bg-white shadow-md rounded px-8 pt-3 mt-3 pb-20 mb-4"
+          type="text"
+          id="body"
+          name="body"
+          placeholder="Enter text here..."
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+      <div className="w-1/2 p-8 flex flex-col">
+        <label htmlFor="imageUrl">Image URL:</label>
+        <input
+          className="bg-white shadow-md rounded px-8 pt-3 pb-20 mt-3 mb-4"
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          placeholder="Image URL"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <PictureInput
+          pictures={""}
+          handleChange={function (
+            e: React.ChangeEvent<HTMLInputElement>
+          ): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+        <button
+          type="button" // Use type="button" to prevent form submission
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover-bg-blue-700 focus:outline-none focus:shadow-outline"
+          onClick={handleCallNotifications} // Call the function when the button is clicked
+        >
+          Send Notification
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
--- pages/landing_page.tsx
+++ pages/landing_page.tsx
@@ -1,48 +1,57 @@
-import React from 'react';
+import React from "react";
 
 function LandingPage() {
-    return ( 
-        <div className="bg-gray-100 min-h-screen"> 
-            {/*page header*/}
-            <header className="bg-emerald-900 text-white py-8">
-                <div className="container mx-auto flex justify-between items-center">
-                    <div>
-                        <h1 className="text-4xl font-bold">Admin Portal</h1>
-                        <p className="mt-2 text-lg">Manage all your admin tasks in a single place.</p>
-                    </div>
-                    <nav>
-                        <ul className="flex space-x-6">
-                            <li>
-                                <a href="./HomePage">Cards Home</a>
-                            </li>
-                            <li>
-                                <a href="./EditModal">Edit</a>
-                            </li>
-                            <li>
-                                <a href="./index">Index</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-
-            {/*page body*/}
-            <main className="container mx-auto py-16">
-                <div className="text-center">
-                    <h2 className="text-2xl font-semibold">Get Started</h2>
-                    <p className="mt-4 text-gray-600">Sign in to access your admin dashboard.</p>
-                </div>
-
-                {/*sign in button*/}
-                <div className="mt-8 flex justify-center">
-                    <a
-                        href="./AuthForm" 
-                        className="bg-emerald-900 hover:bg-emerald-700 text-white font-semibold py-2 px-6 rounded transition duration-300"
-                    >Sign In</a>
-                </div>
-            </main>
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      {/*page header*/}
+      <header className="bg-emerald-900 text-white py-8">
+        <div className="container mx-auto flex justify-between items-center">
+          <div>
+            <h1 className="text-4xl font-bold">Admin Portal</h1>
+            <p className="mt-2 text-lg">
+              Manage all your admin tasks in a single place.
+            </p>
+          </div>
+          <nav>
+            <ul className="flex space-x-6">
+              <li>
+                <a href="./Announcements">Announcements</a>
+              </li>
+              <li>
+                <a href="./HomePage">Cards Home</a>
+              </li>
+              <li>
+                <a href="./EditModal">Edit</a>
+              </li>
+              <li>
+                <a href="./index">Index</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-    )
+      </header>
+
+      {/*page body*/}
+      <main className="container mx-auto py-16">
+        <div className="text-center">
+          <h2 className="text-2xl font-semibold">Get Started</h2>
+          <p className="mt-4 text-gray-600">
+            Sign in to access your admin dashboard.
+          </p>
+        </div>
+
+        {/*sign in button*/}
+        <div className="mt-8 flex justify-center">
+          <a
+            href="./AuthForm"
+            className="bg-emerald-900 hover:bg-emerald-700 text-white font-semibold py-2 px-6 rounded transition duration-300"
+          >
+            Sign In
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default LandingPage;
