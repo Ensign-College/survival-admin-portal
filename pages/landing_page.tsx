b0539e11cae6c5b1d5a136879c90f6626926f84e
Iana Sanduta <80233893+IanaSanduta@users.noreply.github.com>
2023-10-25 15:42:22 -0600

    Login page (#50)
    * Added a login page UI
    
    * Added tcp connection
    
    Added tcp connection
    
    * fix: Added logo to the page
    
    * fix: merged main and resolved conflicts
    
    * feat: edited the landing to have the toogle and fixed the logo on login
    
    * fix: deleted dist folder

--- pages/landing_page.tsx
+++ pages/landing_page.tsx
@@ -85,7 +85,7 @@ function LandingPage() {
         {/*sign in button*/}
         <div className="mt-8 flex justify-center">
           <a
-            href="./AuthForm"
+            href="./Login"
             className="bg-emerald-900 hover:bg-emerald-700 text-white font-semibold py-2 px-6 rounded transition duration-300"
           >
             Sign In
