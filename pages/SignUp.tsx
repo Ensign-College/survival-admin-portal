655f0c4e2cda88e824f2f652405f1df220e31ab0
Jose Yanez <yanezhjs@gmail.com>
2023-12-02 13:45:20 -0700

    made it look nicer

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -40,33 +40,96 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Fullname"
-          name="fullName"
-          onChange={handleChange}
-          value={formData.fullName}
-        />
-
-        <input
-          placeholder="Email"
-          name="email"
-          onChange={handleChange}
-          value={formData.email}
-        />
-
-        <input
-          placeholder="Password"
-          name="password"
-          type="password"
-          onChange={handleChange}
-          value={formData.password}
-        />
-
-        <button type="submit">Submit</button>
-      </form>
+    <div className="h-screen flex">
+  <div className="w-1/2 bg-white flex flex-col justify-center">
+    <div className="text-center p-10">
+      <img
+        src="/assets/logo.png"
+        className="max-w-full max-h-full mr-4"
+        alt="Logo"
+      />
     </div>
+  </div>
+  <form
+    className="w-1/2"
+    style={{ backgroundColor: "#006241" }}
+    onSubmit={handleSubmit}
+  >
+    {/* Sign-up form takes the whole right side with a background color */}
+    <div className="h-full flex flex-col justify-center items-center">
+      {/* Center the content vertically in the form area */}
+      <h2 className="text-2xl font-semibold mb-2 text-white text-center">
+        Sign Up
+      </h2>{" "}
+      {/* Center the "Sign Up" header both horizontally and vertically */}
+      <div className="space-y-4 w-6/12 ">
+        <div>
+          <label
+            htmlFor="fullName"
+            className="block text-sm font-medium text-gray-700 text-white"
+          >
+            Fullname
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            onChange={handleChange}
+            value={formData.fullName}
+            className="w-full border rounded-md py-2 px-3"
+            placeholder="Enter your fullname"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700 text-white"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
+            className="w-full border rounded-md py-2 px-3"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700 text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+            className="w-full border rounded-md py-2 px-3"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <div className="text-center">
+          {/* Center the content horizontally */}
+          <button
+            type="submit"
+            className="w-full bg-[#006241] text-white font-semibold py-2 rounded-md hover:bg-blue-600 border border-white"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
   );
 };
 
